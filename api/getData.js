0fb53a1f5fbc45c06be0cc3930ed9afb35de2740
sumin <sumin@mission.law>
2025-10-16 12:37:10 -0700

    fix: fix sec env airtable name

--- api/getData.js
+++ api/getData.js
@@ -1,13 +1,15 @@
 export default async function handler(req, res) {
   const { inboxId } = req.query;
-  const apiUrl = process.env.VITE_AIRTABLE_API_URL;
+  const apiUrl = process.env.AIRTABLE_API_URL;
   const secret = process.env.API_SECRET_KEY;
   const headers = { 'Authorization': `Bearer ${secret}` };
 
+  /*
   // ADD THESE LINES TO DEBUG
   console.log('Proxy is calling API at:', apiUrl);
   console.log('Sending Headers:', JSON.stringify(headers, null, 2));
-  
+  */
+
   try {
     const apiResponse = await fetch(`${apiUrl}/search-by-inbox?inboxId=${inboxId}`, {
       headers: headers
